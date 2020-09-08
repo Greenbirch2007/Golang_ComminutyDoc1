80249e9dfab81c5d1cbc2af8fc3a2432d36401de
Greenbirch2007 <291109028@qq.com>
2020-09-08 21:36:40 +0800

    9.8 chg

--- g.go
+++ g.go
@@ -2,29 +2,90 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup
+/*
+goroutine和channel结合解决两个问题
+1. 如果变化开启的协程数，4 6 9 如何简化算法
+2. 想一遍统计一遍打印 统计和打印素数并行执行
+单独的goroutine无法实现数据的共享
 
-func test() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("你好 golang", i)
-		time.Sleep(time.Millisecond * 100)
+*/
+
+var wg sync.WaitGroup
+func putNum(intChan chan int){
+	for i:= 2;i<12000;i++{
+		intChan <- i
 	}
-	wg.Done() //协程计数器减一
+	close(intChan)
+	wg.Done()
 }
 
-func main() {
-	wg.Add() //协程计数器加一
 
-	go test() //开启一个协程(就是用户级别的方法)
-	for i := 0; i < 10; i++ {
-		fmt.Println("hello golang", i)
-		time.Sleep(time.Millisecond * 50)
+func printPrime(primeChan chan int){
+	for v := range primeChan{
+		fmt.Println(v)
 	}
-	wg.Wait（）
-	fmt.Println("主线程退出")
+	wg.Done()
+}
+
+func primeNum(intChan chan int,primeChan chan int,exitChan chan bool){
+	for num := range intChan{
+		var flag = true
+		for i :=2;i<num;i++{
+			if num %i ==0{
+				flag=false
+				break
+			}
+		}
+		if flag {
+			primeChan <- num //num是素数
+		}
+
+	}
+	//要关闭primeChan
+	//close(primeChan) //如果一个cahnnel关闭了就无法给这个channel发送数据了
+	//什么时候关闭primeChan
+	exitChan <- true
+	wg.Done()
+	//执行完一次后，给exitChan里面放入一条数据
+
+}
+
+
+
+func main(){
+	start := time.Now().Unix()
+	intChan := make(chan int,1000)
+	primeChan :=make(chan int,1000)
+	exitChan := make(chan bool,16) //标识primeChan close
+	//存放数字的协程
+	wg.Add(1)
+	go putNum(intChan)
+	for i:=0;i<16 ;i++  {
+		wg.Add(1)
+		//统计素数的协程
+		go primeNum(intChan,primeChan,exitChan)
+	}
+	//打印素数的协程
+	wg.Add(1)
+	go printPrime(primeChan)
+	//判断exitChan是否存满
+	wg.Add(1)
+	go func() {
+		for i:=0;i<16;i++{
+			//取值
+			<- exitChan
+		}
+		//关闭primeChan
+		close(primeChan)
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Println("执行完毕")
+	end := time.Now().Unix()
+	fmt.Println(end-start)
 
 }
